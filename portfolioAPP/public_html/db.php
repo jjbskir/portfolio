0f45c2d979cf8714f7a7af86f0ef27054c287868
jjbskir <jjbskir@gmail.com>
2013-04-16 14:24:08 -0600

    db

---
+++ portfolioAPP/public_html/db.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * db config file
+ */
+
+$dbConfig = array(
+    'driver'    => 'pdo_mysql',
+    'dbname'    => 'madeline_Portfolio',
+    'host'      => '127.0.0.1',
+    'user'      => 'madeline_root',
+    'password'  => 'Webmaster1!'
+);
+
+return $dbConfig;
+
+?>
