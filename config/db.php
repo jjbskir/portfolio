44b6340bb09c57ad796d3c1bfb9ba85176dbce58
jjbskir <jjbskir@gmail.com>
2013-04-20 21:42:44 -0600

    db

--- config/db.php
+++ config/db.php
@@ -8,8 +8,8 @@ $dbConfig = array(
     'driver'    => 'pdo_mysql',
     'dbname'    => 'Portfolio',
     'host'      => '127.0.0.1',
-    'user'      => 'root',
-    'password'  => 'Lakess1'
+    'user'      => 'username',
+    'password'  => 'password'
 );
 
 return $dbConfig;
